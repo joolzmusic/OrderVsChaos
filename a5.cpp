4f31c861d81e6c097b63c9d808f089827a94e229
Nat H <74020864+MxSelfDestruct@users.noreply.github.com>
2023-03-24 12:19:55 -0700

    Add files via upload

--- a5.cpp
+++ a5.cpp
@@ -37,55 +37,49 @@ enum Game_State { INCOMPLETE, ORDER_WIN, CHAOS_WIN };
 
 class Board {
 private:
-    char** _board/*[9][9]*/;
-    ushort _width;
-    ushort _height;
+    char** _board;
+    ushort _rows;
+    ushort _cols;
 
 public:
-    Board(ushort x, ushort y): _board(nullptr), _width(x), _height(y) {
-        if (x > 9 || x < 1 || y > 9 || y < 1) {
+    Board(ushort x, ushort y): _board(nullptr), _rows(y), _cols(x) {
+        if (x > 9 || x < 6 || y > 9 || y < 6) {
             throw runtime_error("Tried to make a board with dimensions outside 1x1 to 9x9!");
-            _width = 9;
-            _height = 9;
+            _rows = 6;
+            _cols = 6;
         }
 
         // Frankly unnecessary but I wanted to practice dynamic allocation
-        _board = new char*[_width];
-        for (ushort i = 0; i < _width; i++) {
-            _board[i] = new char[_height];
-            for (ushort j = 0; j < _height; j++) _board[i][j] = '_'; // Don't want garbage data
+        _board = new char*[_rows];
+        for (ushort i = 0; i < _rows; i++) {
+            _board[i] = new char[_cols];
+            for (ushort j = 0; j < _cols; j++) _board[i][j] = '.'; // Don't want garbage data!
         }
-
-        /*
-        for (ushort i = 0; i < _width; i++) {
-            for (ushort j = 0; j < _height; j++) _board[i][j] = '_';
-        }
-        */
     }
 
     ~Board() {
-        for (ushort i = 0; i < _width; i++) delete[] _board[i];
+        for (ushort i = 0; i < _rows; i++) delete[] _board[i];
         delete[] _board;
     }
 
-    const ushort& width = _width;
-    const ushort& height = _height;
-    char** board = _board; // Be careful! Look - don't touch!
+    const ushort& width = _cols;
+    const ushort& height = _rows;
+    // char** board = _board; // Be careful! Look - don't touch!
 
     void verify_board() {
-        for (ushort i = 0; i < _height; i++)
-            for (ushort j = 0; j < _width; j++)
-                if (_board[j][i] != 'X' && _board[j][i] != 'O' && _board[j][i] !=  '_') 
+        for (ushort y = 0; y < height; y++)
+            for (ushort x = 0; x < width; x++)
+                if (_board[y][x] != 'X' && _board[y][x] != 'O' && _board[y][x] !=  '.')
                     throw runtime_error("Invalid board!");
     }
 
     char get_tile(ushort x, ushort y) {
-        if (x >= _width || y >= _height) {
-            throw runtime_error("Tried to get tile out of bounds!");
+        if (x >= width || y >= height) {
+            throw runtime_error("Tried to get cell out of bounds!");
             return '\0';
         }
 
-        return _board[x][y];
+        return _board[y][x];
     }
 
     // Returns 1 if the piece was successfully added, 0 if not.
@@ -93,47 +87,77 @@ public:
     // exception is thrown, which happens if X exceeds _width - 1, Y exceeds _height - 1, or if
     // C is not 'X' or 'O'.
     bool add_piece(char c, ushort x, ushort y) {
-        if (x >= _width || y >= _height) {
-            throw runtime_error("Tried to set to tile out of bounds!");
-            return 0;
+        if (x >= width || y >= height) {
+            throw runtime_error("Tried to set to cell out of bounds!");
+            return false;
         }
 
         if (c != 'X' && c != 'O') {
             throw runtime_error("Tried to set invalid piece! (Must be X or O)");
-            return 0;
+            return false;
         }
 
-        if (_board[x][y] == 'X' || _board[x][y] == 'O') return 0;
+        if (_board[y][x] == 'X' || _board[y][x] == 'O') return false;
 
-        _board[x][y] = c;
-        return 1;
+        _board[y][x] = c;
+        return true;
     }
 
     Game_State check_gamestate() {
         verify_board(); // Can't be too safe!
-        // Check for a row column, or cross of 5 'X's or 'O's
+        // Check for a row, column, or cross of 5 'X's or 'O's
+        // Row check
+        for (ushort y = 0; y < height; y++) {
+            ushort consecutive = 0;
 
-        // Check if all board spaces are filled
-        for (ushort i = 0, spaces_used = 0; i < _width; i++) {
-            for (ushort j = 0; j < _height; j++) {
-                if (_board[j][i] == 'X' || _board[j][i] == 'Y') spaces_used++;
+            for (ushort x = 1; x < width; x++) {
+                if (_board[y][x] == _board[y][x - 1] && _board[y][x] != '.') consecutive++;
             }
 
-            if (spaces_used == _width * _height) return CHAOS_WIN;
+            if (consecutive >= 4) return ORDER_WIN;
+        }
+
+        // Column check
+        for (ushort x = 0; x < width; x++) {
+            ushort consecutive = 0;
+
+            for (ushort y = 1; y < height; y++) {
+                if (_board[y][x] == _board[y - 1][x] && _board[y][x] != '.') consecutive++;
+            }
+
+            if (consecutive >= 4) return ORDER_WIN;
+        }
+
+        // Cross (\) check
+        
+
+        // Cross (/) check
+
+        // Check if all board spaces are filled
+        ushort spaces_used = 0;
+        for (ushort y = 0; y < height; y++) {
+            for (ushort x = 0; x < width; x++) {
+                if (_board[y][x] == 'X' || _board[y][x] == 'Y') spaces_used++;
+            }
+
+            if (spaces_used == width * height) return CHAOS_WIN;
         }
 
         return INCOMPLETE;
     }
 
     void print_board() {
-        cout << ' ';
-        for (ushort i = 0; i < _height; i++) cout << ' ' << i;
+        cout << "  ";
+        for (ushort x = 0; x < width; x++) cout << x + 1 << ' ';
         cout << '\n';
-        for (ushort i = 0; i < _height; i++) {
-            cout << static_cast<char>(i + 'A');
-            for (ushort j = 0; j < _width; j++) cout << ' ' << _board[j][i];
+
+        for (ushort y = 0; y < height; y++) {
+            cout << static_cast<char>('A' + y) << ' ';
+            for (ushort x = 0; x < width; x++) cout << _board[y][x] << ' ';
             cout << '\n';
         }
+
+        cout << '\n';
     }
 };
 
@@ -142,17 +166,18 @@ bool AI_chaos(Board& board);
 bool AI_dummy(Board& board);
 
 int main() {
-    Board board(9, 9);
+    Board board(8, 6);
     board.print_board();
 
     // Extremely incomplete
     while (board.check_gamestate() == INCOMPLETE) {
         string player_move = "";
-        cout << "Enter a move [numberLETTER(X/O)]: ";
+        cout << "Enter a move [NumberLetter(X/O)]: ";
         cin >> player_move;
-        ushort x = player_move.at(0) - '0';
-        ushort y = player_move.at(1) - 'A';
-        char c = player_move.at(2);
+
+        char c = toupper(player_move.at(2));
+        ushort x = player_move.at(0) - '0' - 1;
+        ushort y = toupper(player_move.at(1)) - 'A';
 
         board.add_piece(c, x, y);
         board.print_board();
@@ -168,14 +193,14 @@ Assignment 5 Report
 Description of Computers Playing Strategy
 -----------------------------------------
 AI_order:
-    TBA. If playing first, always takes a centre tile with a random piece.
+    TBA. If playing first, always takes a centre cell with a random piece.
 
 AI_chaos:
     A contrarian. Searches for the longest row/column/cross of the same pieces, and places the
-    opposite piece at one end. If playing first, always takes a centre tile with a random piece.
+    opposite piece at one end. If playing first, always takes a centre cell with a random piece.
 
 AI_dummy:
-    Always places a random piece on a random tile. Rather boring.
+    Always places a random piece on a random cell. Rather boring.
 
 Extra Features
 --------------
