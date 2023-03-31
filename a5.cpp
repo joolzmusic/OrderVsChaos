f9120293c0ad30fca37d4466ca5383a5e5632834
Nat H <74020864+MxSelfDestruct@users.noreply.github.com>
2023-03-31 16:17:17 -0700

    Many updates!
    * Game loop now accounts for either Order OR Chaos going first
    * Player 1 is chosen at random irrespective of their role
    * Enums are now classes to satisfy project requirement
    * Improved input validation
    * Game now ends immediately if player 1 plays a winning move

--- a5.cpp
+++ a5.cpp
@@ -35,8 +35,8 @@
 
 using namespace std;
 
-enum Player_Role { ORDER, CHAOS };
-enum Game_State { INCOMPLETE, ORDER_WIN, CHAOS_WIN };
+enum class Player_Role { ORDER, CHAOS };
+enum class Game_State { INCOMPLETE, ORDER_WIN, CHAOS_WIN };
 
 struct Point { ushort x, y; };
 struct Move { char piece; Point cell; };
@@ -59,6 +59,14 @@ public:
         for (ushort y = 0; y < _size; y++) {
             _board[y] = new char[_size];
             for (ushort x = 0; x < _size; x++) _board[y][x] = '*'; // Don't want garbage data!
+        }
+    }
+
+    Board(const Board& b): _board(nullptr), _size(b.size), _used(b.used) {
+        _board = new char*[_size];
+        for (ushort y = 0; y < _size; y++) {
+            _board[y] = new char[_size];
+            for (ushort x = 0; x < _size; x++) _board[y][x] = b.get_cell(x, y);
         }
     }
 
@@ -113,7 +121,7 @@ public:
                     for (ushort i = y, len = 1; i < size - 1; i++) {
                         if (_board[i][x] == _board[i + 1][x]) len++;
                         else break;
-                        if (len >= 5) return ORDER_WIN;
+                        if (len >= 5) return Game_State::ORDER_WIN;
                     }
                 }
 
@@ -122,7 +130,7 @@ public:
                     for (ushort i = x, len = 1; i < size - 1; i++) {
                         if (_board[y][i] == _board[y][i + 1]) len++;
                         else break;
-                        if (len >= 5) return ORDER_WIN;
+                        if (len >= 5) return Game_State::ORDER_WIN;
                     }
                 }
 
@@ -131,7 +139,7 @@ public:
                     for (ushort i = x, j = y, len = 1; i < size - 1 && j < size - 1; i++, j++) {
                         if (_board[j][i] == _board[j + 1][i + 1]) len++;
                         else break;
-                        if (len >= 5) return ORDER_WIN;
+                        if (len >= 5) return Game_State::ORDER_WIN;
                     }
                 }
 
@@ -140,13 +148,13 @@ public:
                     for (ushort i = x, j = y, len = 1; i > 0 && j < size - 1; i--, j++) {
                         if (_board[j][i] == _board[j + 1][i - 1]) len++;
                         else break;
-                        if (len >= 5) return ORDER_WIN;
+                        if (len >= 5) return Game_State::ORDER_WIN;
                     }
                 }
         }
         
         // If all cells are used, Chaos wins. If not, the game is incomplete.
-        return used == area ? CHAOS_WIN : INCOMPLETE;
+        return used == area ? Game_State::CHAOS_WIN : Game_State::INCOMPLETE;
     }
 
     // Returns a heap-allocated array of all free cells on the board. You must free this!
@@ -193,94 +201,117 @@ public:
     }
 };
 
-Move prompt_player_move(const char* prompt, Board& board); // Returns an invalid move if the player concedes
-Move ai_random(Board& board);
-Move ai_clever(Board& board);
+Move prompt_player_move(const char* prompt, const Board& board);
+Move ai_random(const Board& board);
+Move ai_clever(const Board& board);
 
 int main(void) {
-    srand(time(NULL));
+    srand(time(nullptr));
+
     ushort board_size = 0;
     uint turn_count = 0;
-    Player_Role human_player_role = rand() % 2 ? ORDER : CHAOS;
+    const Player_Role human_player_role = rand() % 2 ? Player_Role::ORDER : Player_Role::CHAOS;
+    const Player_Role first_player = rand() % 2 ? Player_Role::ORDER : Player_Role::CHAOS;
 
     cout << "Welcome to Order and Chaos!\n";
     // TODO: Add game rules
 
-    while(board_size < 6 || board_size > 9) {
+    while (board_size < 6 || board_size > 9) {
         cout << "Please choose appropriate size for board game: 6, 7, 8, or 9: ";
         cin >> board_size;
-        if (board_size >= 6 || board_size <= 9) break;
+        if (board_size >= 6 && board_size <= 9) break;
         cout << "Invalid input.\n";
     }
 
     Board game(board_size);
     Game_State gs = game.check_gamestate();
-    cout << "You are playing as " << (human_player_role == ORDER ? "Order" : "Chaos");
-    cout << " - Order to play first.\n";
-    if (human_player_role == ORDER) game.print_board(); // Show a blank board if human goes first
+
+    cout << "You are playing as " << (human_player_role == Player_Role::ORDER ? "Order" : "Chaos")
+         << (first_player == human_player_role ? " - Human" : " - Computer") << " plays first.\n";
+
+    // Show a blank board if human goes first
+    if (first_player == human_player_role) game.print_board();
     cout << '\n';
 
-    while (gs == INCOMPLETE) {
-        Move order_move = human_player_role == ORDER ? 
+    while (gs == Game_State::INCOMPLETE) {
+        // Player 1 turn
+        if (first_player == human_player_role) 
+            cout << (human_player_role == Player_Role::ORDER ? "[ORDER]: " : "[CHAOS]: ");
+
+        Move player_1_move = human_player_role == first_player ?
             prompt_player_move("Enter a move [NumberLetter(X/O)]: ", game) : ai_random(game);
 
-        if (order_move.piece == '\0') {
+        if (player_1_move.piece == '\0') {
             cout << "You conceded. You lost in " << turn_count << " turn(s). Good game.\n";
             return 0;
         }
 
-        if (human_player_role != ORDER) {
-            cout << "Computer plays " << order_move.piece << " at ";
-            cout << order_move.cell.x + 1 << (char)(order_move.cell.y + 'A') << '\n';
+        if (human_player_role != first_player) {
+            cout << "Computer plays " << player_1_move.piece << " at "
+                 << player_1_move.cell.x + 1 << (char)(player_1_move.cell.y + 'A') << '\n';
         }
 
-        game.add_piece(order_move);
+        game.add_piece(player_1_move);
         game.print_board();
         cout << '\n';
 
-        Move chaos_move = human_player_role == CHAOS ? 
-            prompt_player_move("Enter a move [NumberLetter(X/O)]: ", game) : ai_random(game);
-
-        if (order_move.piece == '\0' || chaos_move.piece == '\0') {
-            cout << "You conceded. You lost in " << turn_count << " turn(s). Good game.\n";
-            return 0;
-        }
-
-        if (human_player_role != CHAOS) {
-            cout << "Computer plays " << order_move.piece << " at ";
-            cout << order_move.cell.x + 1 << (char)(order_move.cell.y + 1 + 'A') << '\n';
-        }
-
-        game.add_piece(chaos_move);
-        game.print_board();
-        cout << '\n';
-        
-        turn_count++;
         gs = game.check_gamestate();
+        if (gs != Game_State::INCOMPLETE) break;
 
-        switch (gs) {
-            case ORDER_WIN:
-                cout << "Order wins!\n";
-                cout << (human_player_role == ORDER ? "You won" : "You lost");
-                cout << " in " << turn_count << " turns. Good game";
-                cout << (human_player_role == ORDER ? ", well played." : ".") << '\n';
-                break;
-            
-            case CHAOS_WIN:
-                cout << "Chaos wins!\n";
-                cout << (human_player_role == CHAOS ? "You won" : "You lost");
-                cout << " in " << turn_count << " turns. Good game";
-                cout << (human_player_role == CHAOS ? ", well played." : ".") << '\n';
-                break;
-            
-            default: break;
+        // Player 2 turn
+        if (first_player != human_player_role)
+            cout << (human_player_role == Player_Role::ORDER ? "[ORDER]: " : "[CHAOS]: ");
+
+        Move player_2_move = human_player_role != first_player ?
+            prompt_player_move("Enter a move [NumberLetter(X/O)]: ", game) : ai_random(game);
+
+        if (player_1_move.piece == '\0' || player_2_move.piece == '\0') {
+            cout << "You conceded. You lost in " << turn_count << " turn(s). Good game.\n";
+            return 0;
         }
+
+        if (human_player_role == first_player) {
+            cout << "Computer plays " << player_1_move.piece << " at "
+                 << player_1_move.cell.x + 1 << (char)(player_1_move.cell.y + 1 + 'A') << '\n';
+        }
+
+        game.add_piece(player_2_move);
+        game.print_board();
+        cout << '\n';
+
+        gs = game.check_gamestate();
+        turn_count++;
     }
+
+    switch (gs) {
+        case Game_State::ORDER_WIN:
+            cout << "Order wins!\n"
+                 << (human_player_role == Player_Role::ORDER ? "You won" : "You lost") << " in "
+                 << turn_count << " turns. Good game" 
+                 << (human_player_role == Player_Role::ORDER ? ", well played!" : ".") << '\n';
+            break;
+        
+        case Game_State::CHAOS_WIN:
+            cout << "Chaos wins!\n"
+                 << (human_player_role == Player_Role::CHAOS ? "You won in " : "You lost in ")
+                 << turn_count << " turns. Good game" 
+                 << (human_player_role == Player_Role::CHAOS ? ", well played!" : ".") << '\n';
+            break;
+        
+        default: break;
+    }
+
+    char play_again = 'N';
+    cout << "Play again? (y/N): ";
+    cin >> play_again;
+    if (play_again == 'y' || play_again == 'Y') return main();
     
     return 0;
 }
 
-Move prompt_player_move(const char* prompt, Board& board) {
+// Prompts the player for a move and returns it, repeadedly asking until a valid move is entered.
+// Returns a move with piece '\0' if the player quits.
+Move prompt_player_move(const char* prompt, const Board& board) {
     string player_move_str = "";
     Move player_move = {'\0', {0, 0}};
 
@@ -321,10 +352,10 @@ Move prompt_player_move(const char* prompt, Board& board) {
 
 // Loads the coordinates of each empty cell on the board and places a random piece on one of them.
 // Returns the coordinates of the piece placed.
-Move ai_random(Board& board) {
+Move ai_random(const Board& board) {
     if (board.used == board.area) {
         throw runtime_error("Tried to play on a full board!");
-        return {'X', {0, 0}};
+        return {'\0', {0, 0}};
     }
 
     Point* free_cells = board.get_free_cells();
@@ -338,10 +369,10 @@ Move ai_random(Board& board) {
     return m;
 }
 
-Move ai_clever(Board& board) {
+Move ai_clever(const Board& board) {
     if (board.used == board.area) {
         throw runtime_error("Tried to play on a full board!");
-        return {'O', {0, 0}};
+        return {'\0', {0, 0}};
     }
 
     Point* free_cells = board.get_free_cells();
@@ -358,7 +389,8 @@ Assignment 5 Report
 Description of Computers Playing Strategy
 -----------------------------------------
 ai_random:
-    Always places a random piece on a random open cell. Rather boring.
+    Given an array of empty cells on a board, it will choose one at random and place a random
+    piece on it.
 
 ai_clever:
     Looks for the longest sequence of pieces in a row, column, or diagonal. If it finds one, it
